8e08b8ee2e2b2cb63c7a5abe21af7d8d1f9bd23c
bloodysysy73 <sylvain.guehria@gmail.com>
2020-11-30 19:12:31 +0100

    styling table subtask

--- src/components/tables/SimpleTableLvl1.vue
+++ src/components/tables/SimpleTableLvl1.vue
@@ -2,26 +2,45 @@
   <div>
     <md-table class="table-custom no-border">
       <md-table-row v-if="item.subtasks && item.subtasks.length">
-        <md-table-head width="50px">Order</md-table-head>
+        <md-table-head width="40px"></md-table-head>
         <md-table-head width="150px">Label</md-table-head>
-        <md-table-head width="150px">Details</md-table-head>
+        <md-table-head width="250px">Details</md-table-head>
         <md-table-head width="50px">Deadline</md-table-head>
         <md-table-head width="50px">Importance</md-table-head>
+        <md-table-head width="50px">Order</md-table-head>
         <md-table-head width="50px">Done</md-table-head>
         <md-table-head width="50px">Delete/Edit</md-table-head>
       </md-table-row>
 
       <md-table-row v-for="(subtask, index) in item.subtasks" :key="index">
-        <md-table-head width="50px">{{ subtask.order }}</md-table-head>
-        <md-table-head width="150px">{{ subtask.label }}</md-table-head>
-        <md-table-head width="150px">{{ subtask.detail }}</md-table-head>
+        <md-table-cell width="40px"></md-table-cell>
+        <md-table-cell width="150px">
+          <div class="reliure"></div>
+          <div class="dot"></div>
+          <div class="label">
+            <p>{{ subtask.label }}</p>
+          </div></md-table-cell
+        >
+        <md-table-cell width="250px"
+          ><p>{{ subtask.detail }}</p></md-table-cell
+        >
         <md-table-head width="50px">{{ subtask.deadline }}</md-table-head>
         <md-table-head width="50px">{{ subtask.importance }}</md-table-head>
+        <md-table-head width="50px">{{ subtask.order }}</md-table-head>
 
-        <md-table-head 
-        width="50px"
-          ><feather type="check" class="hover-click" v-if="subtask.isdone"  @click="setSubTaskState(subtask.key, item.key, false)"></feather>
-          <feather type="x" class="hover-click" v-if="!subtask.isdone"  @click="setSubTaskState(subtask.key, item.key, true)"></feather>
+        <md-table-head width="50px"
+          ><feather
+            type="check"
+            class="hover-click"
+            v-if="subtask.isdone"
+            @click="setSubTaskState(subtask.key, item.key, false)"
+          ></feather>
+          <feather
+            type="x"
+            class="hover-click"
+            v-if="!subtask.isdone"
+            @click="setSubTaskState(subtask.key, item.key, true)"
+          ></feather>
         </md-table-head>
 
         <md-table-head width="50px">
@@ -37,16 +56,19 @@
             @click="deleteSubtask(subtask.key, item.key)"
           ></feather>
         </md-table-head>
-
       </md-table-row>
-      <md-table-row >
+      <md-table-row>
         <md-table-head colspan="6">
-          <feather type="plus-circle" class="hover-click" @click="addSubtaskModal(item.key)"></feather
+          <feather
+            type="plus-circle"
+            class="hover-click"
+            @click="addSubtaskModal(item.key)"
+          ></feather
         ></md-table-head>
       </md-table-row>
     </md-table>
 
-    <md-dialog :md-active.sync="showDialogAddSubtask" >
+    <md-dialog :md-active.sync="showDialogAddSubtask">
       <add-subtask-modal
         @closeAddSubtaskModal="closeAddSubtaskModal"
         :motherKey="getMotherKey()"
@@ -76,11 +98,11 @@ export default class SimpleTableLvl1 extends Vue {
 
   motherKey: string = "";
 
-  setSubTaskState(subtaskKey, motherKey, isDone){
-    this.$store.dispatch("setSubTaskState", {subtaskKey, motherKey, isDone} )
+  setSubTaskState(subtaskKey, motherKey, isDone) {
+    this.$store.dispatch("setSubTaskState", { subtaskKey, motherKey, isDone });
   }
 
-  getSubtaskToEdit(){
+  getSubtaskToEdit() {
     return this.subtaskToEdit;
   }
 
@@ -110,35 +132,64 @@ export default class SimpleTableLvl1 extends Vue {
   }
 
   deleteSubtask(subtaskKey: string, todoKey: string) {
-      let keys = {
-        subtaskKey,
-        todoKey
-      }
-      let vm = this;
-      this.$store
-        .dispatch("deleteSubtask", keys)
-        .then(() => {
-          this.$toasted.show("Subtask deleted, it is no longer in your list", {
-            icon: "delete_outline",
-            theme: "bubble",
-            position: "bottom-right",
-            duration: 5000,
-          });
-        })
-        .catch((error: Error) => {
-          this.$toasted.show("Cannot deleted Subtask", {
-            icon: "error_outline",
-            theme: "bubble",
-            position: "bottom-right",
-            duration: 5000,
-          });
+    let keys = {
+      subtaskKey,
+      todoKey,
+    };
+    let vm = this;
+    this.$store
+      .dispatch("deleteSubtask", keys)
+      .then(() => {
+        this.$toasted.show("Subtask deleted, it is no longer in your list", {
+          icon: "delete_outline",
+          theme: "bubble",
+          position: "bottom-right",
+          duration: 5000,
         });
+      })
+      .catch((error: Error) => {
+        this.$toasted.show("Cannot deleted Subtask", {
+          icon: "error_outline",
+          theme: "bubble",
+          position: "bottom-right",
+          duration: 5000,
+        });
+      });
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.hover-click{
+.hover-click {
   cursor: pointer;
+}
+   .label {
+      position: absolute;
+      left: 15px;
+      top: 7px;
+    }
+table {
+  table {
+ 
+    .dot {
+      width: 6px;
+      height: 6px;
+      border-radius: 100%;
+      border: solid grey 1px;
+      background-color: grey;
+      position: absolute;
+      left: 0px;
+      top: 15px;
+    }
+    .reliure {
+      border-left: dotted #a0a2a4 1px;
+      position: absolute;
+      left: 2px;
+      top: 24px;
+
+      width: 1px;
+      height: 47px;
+    }
+  }
 }
 </style>
